54790f0bbcbe2201c4d8b7d4ad382e8eb58e050e
Joaquin <joaquintalice@gmail.com>
2024-01-05 20:47:48 -0300

    feat: error handling with interceptors

---
+++ src/app/core/test-errors/test-errors.component.ts
@@ -0,0 +1,48 @@
+import { HttpClient } from '@angular/common/http';
+import { Component } from '@angular/core';
+import { environment } from 'src/app/environments/environment';
+
+@Component({
+  selector: 'app-test-errors',
+  templateUrl: './test-errors.component.html',
+  styleUrls: ['./test-errors.component.scss']
+})
+export class TestErrorsComponent {
+  baseUrl = environment.apiUrl;
+  validationErrors: string[]= [];
+
+  constructor(private http: HttpClient) {}
+
+  get404Error() {
+    this.http.get(this.baseUrl + 'products/42').subscribe({
+      next: res=> console.log(res),
+      error: error=>console.log(error)
+    })
+  }
+
+  get500Error() {
+    this.http.get(this.baseUrl + 'buggy/servererror').subscribe({
+      next: res=> console.log(res),
+      error: error=>console.log(error)
+    })
+  }
+
+  get400Error() {
+    this.http.get(this.baseUrl + 'buggy/badrequest').subscribe({
+      next: res=> console.log(res),
+      error: error=>console.log(error)
+    })
+  }
+
+  get400ValidationError() {
+    this.http.get(this.baseUrl + 'products/fortytwo').subscribe({
+      next: res=> console.log(res),
+      error: error=> {
+        console.log(error)
+        this.validationErrors = error.errors;
+        console.log(this.validationErrors)
+      }
+    })
+  }
+
+}
