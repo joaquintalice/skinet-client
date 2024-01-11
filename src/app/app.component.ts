4086d56ccd4d15b1fbb3cfd4c6079bcc8c401a22
Joaquin <joaquintalice@gmail.com>
2024-01-10 23:53:53 -0300

    feat: basket entire feature

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,18 +2,22 @@ import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { Product } from './shared/models/product';
 import { Pagination } from './shared/models/pagination';
+import { BasketService } from './basket/basket.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'skinet';
 
-  constructor(private http: HttpClient) { }
+  constructor(private basketService: BasketService) { }
 
-
+  ngOnInit(): void {
+    const basketId = localStorage.getItem('basket_id');
+    if(basketId) this.basketService.getBasket(basketId)
+  }
 
 }
 
